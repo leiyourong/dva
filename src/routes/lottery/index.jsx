4a7786e6711c84f2ffc08219bc73ac23563ed1ea
leiyourong <460951178@qq.com>
2018-12-27 01:04:37 +0800

    lottery init

--- src/routes/lottery/index.jsx
+++ src/routes/lottery/index.jsx
@@ -3,7 +3,10 @@ import style from './index.less'
 export default () => {
   return <div className={style.lottery}>
     {
-      [1,2,3,4,5,6,7,8,9].map(num => <div key={num} className={style.card}>{num}</div>)
+      [1,2,3,4,5,6,7,8,9].map(num => <div key={num} className={style.card}>
+        <div className={`${style.front} ${style.cardItem}`}></div>
+        <div className={`${style.back} ${style.cardItem}`}>奖品{num}</div>
+      </div>)
     }
   </div>
 }
